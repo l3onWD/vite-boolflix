1004fa5ce2618b3676d5a0f9bee96b58f8dda02b
l3on <l3onwebdev@gmail.com>
2023-07-05 15:41:38 +0200

    added search form component

---
+++ src/components/base/BaseSerachForm.vue
@@ -0,0 +1,24 @@
+<script>
+export default {
+    data() {
+        return {
+            searchedTerm: ''
+        };
+    },
+    props: {
+        placeholder: String
+    },
+    emits: ['form-submit']
+}
+</script>
+
+
+<template>
+    <form @submit.prevent="$emit('form-submit', searchedTerm)">
+        <input v-model.trim="searchedTerm" type="text" :placeholder="placeholder || 'Search...'">
+        <button>Cerca</button>
+    </form>
+</template>
+
+
+<style></style>
