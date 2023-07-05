1e49e4167abb9978bac74aa4410af2aaca2ad3d0
l3on <l3onwebdev@gmail.com>
2023-07-05 16:06:31 +0200

    added movies data to main

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,16 +1,31 @@
 <script>
+/* -----------------------------------------
+* RESOURCES
+-------------------------------------------*/
+/*** DATA ***/
+import { store } from '../data/store';
+
+
 export default {
-
-
+    data: () => store
 }
 </script>
 
 
 <template>
     <main>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio recusandae, ad quam libero impedit,
-            veritatis in quod optio tempore sapiente nostrum corrupti? Natus inventore a officia quam! Consequatur, dolor
-            odit.</p>
+
+        <p v-if="!movies.length">Cerca un film o una serie TV per iniziare.</p>
+
+        <ul v-else>
+            <li v-for="movie in movies" :key="movie.id">
+                <p>Titolo: {{ movie.title }}</p>
+                <p>Titolo Originale: {{ movie.originalTitle }}</p>
+                <p>Lingua: {{ movie.originalLanguage }}</p>
+                <p>Voto: {{ movie.voteAverage }}</p>
+            </li>
+        </ul>
+
     </main>
 </template>
 
