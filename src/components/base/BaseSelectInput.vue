4fd6b8b095835606632ce0e0013c7c313a0499c4
l3on <l3onwebdev@gmail.com>
2023-07-06 17:01:46 +0200

    abstracted base select props keys

--- src/components/base/BaseSelectInput.vue
+++ src/components/base/BaseSelectInput.vue
@@ -1,18 +1,26 @@
 <script>
 export default {
+    data() {
+        return {
+            selectedValue: ''
+        };
+    },
+
     props: {
-        defaultValue: '',
+        defaultLabel: '',
         options: Array
-    }
+    },
+
+    emits: ['select-changed']
 
 }
 </script>
 
 
 <template>
-    <select class="form-select w-auto">
-        <option value="">{{ defaultValue || 'Tutto' }}</option>
-        <option v-for="option in options" :key="option.value" :value="option.value">{{ option.name }}</option>
+    <select v-model="selectedValue" @change="$emit('select-changed', selectedValue)" class="form-select w-auto">
+        <option value="">{{ defaultLabel || 'Tutto' }}</option>
+        <option v-for="option in options" :key="option.value" :value="option.value">{{ option.text }}</option>
     </select>
 </template>
 
