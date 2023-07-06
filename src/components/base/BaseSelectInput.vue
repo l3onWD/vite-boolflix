58e3daef15e7d99ad274c041c06a9b3803cf1f78
l3on <l3onwebdev@gmail.com>
2023-07-06 16:37:56 +0200

    added Select component

---
+++ src/components/base/BaseSelectInput.vue
@@ -0,0 +1,20 @@
+<script>
+export default {
+    props: {
+        defaultValue: '',
+        options: Array
+    }
+
+}
+</script>
+
+
+<template>
+    <select class="form-select w-auto">
+        <option value="">{{ defaultValue || 'Tutto' }}</option>
+        <option v-for="option in options" :key="option.value" :value="option.value">{{ option.name }}</option>
+    </select>
+</template>
+
+
+<style></style>
