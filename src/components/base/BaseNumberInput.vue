d622d72c2b99b840d011601d601fb8f85c7f53ae
l3on <l3onwebdev@gmail.com>
2023-07-07 17:52:37 +0200

    added number input component

---
+++ src/components/base/BaseNumberInput.vue
@@ -0,0 +1,28 @@
+<script>
+export default {
+    data() {
+        return {
+            inputValue: ''
+        };
+    },
+    props: {
+        minValue: Number,
+        maxValue: Number,
+        defaultValue: Number,
+    },
+
+    created() {
+        this.inputValue = this.defaultValue;
+    },
+
+    emits: ['value-changed']
+}
+</script>
+
+
+<template>
+    <input v-model="inputValue" type="number" :min="minValue" :max="maxValue" @change="$emit('value-changed', inputValue)">
+</template>
+
+
+<style></style>
