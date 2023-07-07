c76c4da23a23346ad9d41770d3b178b7e91b92c5
l3on <l3onwebdev@gmail.com>
2023-07-07 19:19:35 +0200

    added no results text

--- src/components/media/MediaSection.vue
+++ src/components/media/MediaSection.vue
@@ -37,11 +37,13 @@ export default {
 
         <h2>{{ title }}</h2>
 
-        <ul class="row g-4 row-cols-2 row-cols-md-3 row-cols-lg-4">
+        <ul v-if="filteredMedia.length" class="row g-4 row-cols-2 row-cols-md-3 row-cols-lg-4">
             <li v-for="media in filteredMedia" :key="media.id" class="col">
                 <MediaCard v-bind="media" />
             </li>
         </ul>
+
+        <p v-else>Nessun risultato trovato.</p>
 
     </section>
 </template>
